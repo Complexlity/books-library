8ced51ad8e414f447a74d7257c61144cd1db9c3c
Complexlity <nwalozielijah@gmail.com>
2022-10-27 20:10:35 +0100

    Complete implementation of add book feature

--- advanced-javascript/main.js
+++ advanced-javascript/main.js
@@ -42,13 +42,14 @@ const addBook = dq('.add-book')
 const bookWrapper = dq('.book-wrapper')
 const addBookSection = dq('.add-book-section')
 const form = dq('.form')
+let checked; 
 addBook.addEventListener('click', openModal)
 getDeleteButtons()
 addBookSection.addEventListener('click', processModal)
 
-form.addEventListener('submit', (e)=>{
-    e.preventDefault()
-})
+
+
+
 
 function Book(title, author, pages, read){
     this.title = title
@@ -67,43 +68,33 @@ function processModal(e){
         e.target.className == 'cancel'
     ){
         addBookSection.style.display = 'none'
+        clearFormSection()
+        renderToPage(library)
     }
 }
 
-/* -------------------
- USING PROMPT TO GET VALUES
- ---------------------- */
-// function addBookToLibrary(){
-//     let title = prompt('What is the title of the book')
-//     let author = prompt('Who is the author')
-//     let pages = prompt('How many pages')
-//     let read = confirm('Have you read it?')
-//     if(!read) read = false
-//     let myNewBook = new Book(title, author, pages, read)
-//     library.push(myNewBook)
-//     renderToPage(library)
-//     getDeleteButtons()
-// }
+
 
 function renderToPage(library){
     bookWrapper.innerHTML = ''
     let book;
     for (let i = 0; i < library.length; i++){
-        book = library[i]   
-    if (book.read) checked = 'checked'
-    bookWrapper.innerHTML += `
-    <div class="book" data-number=${i}>
+        book = library[i]  
+        checked = '' 
+        if (book.read) checked = 'checked'
+        bookWrapper.innerHTML += `
+        <div class="book" data-number=${i}>
         <h1 class="title">${book.title}</h1>
         <p>By</p>
         <p class="author">${book.author}</p>
         <p class="pages">${book.pages}</p>
         <div>
-   
-            <label><input type="checkbox" ${checked}>Read</label>
+        
+        <label><input type="checkbox" ${checked}>Read</label>
         </div>
         <button class="delete">Remove Book</button>
-    `
-}
+        `
+    }
 }
 
 function getDeleteButtons(){
@@ -125,3 +116,84 @@ function removeFromLibrary(){
     }
 }
 
+
+/* -------------------
+ USING PROMPT TO GET VALUES
+ ---------------------- */
+// function addBookToLibrary(){
+//     library.push(myNewBook)
+//     renderToPage(library)
+//     getDeleteButtons()
+// }
+
+/* -----------------------
+FORM VALIDATION SECTION
+------------------------ */
+
+const submit = dq('.add')
+const inputsDiv = dqa('.form-control')
+const inputs = dqa('.form-control input')
+c(inputs)
+let title = inputsDiv[0]
+let titleInput = title.querySelector('input')
+let author = inputsDiv[1]
+let authorInput = author.querySelector('input')
+let pages = inputsDiv[3]
+let pagesInput = pages.querySelector('input')
+let readSwitch = dq('.select')
+let readOrNot = readSwitch.querySelector('select')
+let successCount
+
+c(titleInput, authorInput, pagesInput, readOrNot)
+c(submit)
+submit.addEventListener('click', validateAll)
+form.addEventListener('submit', (e)=>{
+    e.preventDefault()
+})
+
+function validateAll(){
+    successCount = 0;
+    let titleValue = titleInput.value
+    let authorValue = authorInput.value
+    let pagesValue = pagesInput.value
+    let read = readOrNot.value
+    c(read)
+    let readBoolean = (read === '1' || read === '0')  ? true : false
+
+    let error1 = 'Field cannot be left blank'
+    successError(title, Boolean(titleValue), error1)
+    successError(author, Boolean(authorValue), error1)
+    successError(pages, Boolean(pagesValue), error1)
+    successError(readSwitch, readBoolean, 'Choose An Option')
+    if(successCount == 4){
+            let readCheck = read === '1' ? true: false
+            let myNewBook = new Book(titleValue, authorValue, pagesValue, readCheck)
+                library.push(myNewBook)
+                addBookSection.style.display = 'none'
+                clearFormSection()
+                renderToPage(library)
+                getDeleteButtons()
+    }
+}
+
+
+
+
+
+
+// Function to log error messages
+function successError(nodeElement, value, ErrorMessage) {
+    let errorText = nodeElement.querySelector('small')
+    if (value) { nodeElement.classList.add('success'); nodeElement.classList.remove('error')
+    errorText.textContent = ''
+    successCount += 1
+}
+    else {
+        nodeElement.classList.add('error'); nodeElement.classList.remove('success')
+        errorText.textContent = ErrorMessage
+    }
+}
+
+
+function clearFormSection(){
+}
